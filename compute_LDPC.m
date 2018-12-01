b306ceaecb77a7bbe889578b3b331dd4a5d59427
rquinlancmu <44323130+rquinlancmu@users.noreply.github.com>
2018-12-01 16:09:31 -0500

    Add AWGN channel

--- compute_LDPC.m
+++ compute_LDPC.m
@@ -5,12 +5,23 @@ clear all;
 % create initial object
 LDPC_code = LDPC_class(0, 0);
 
-block_length = 1944;
+block_len = 1944;
 rate = 1/2;
 
-LDPC_code.load_parity_matrix(block_length);
-info_length = LDPC_code.info_len;
+LDPC_code.load_parity_matrix(block_len);
+info_len = LDPC_code.info_len;
 
-info_bits = rand(info_length, 1) < 0.5;
+message_bits = rand(info_len, 1) < 0.5;
 %Encode bits
-coded_bits = LDPC_code.encode_bits(info_bits)
+coded_bits = LDPC_code.encode_bits(message_bits)
+
+%flip some bits, add some noise
+flipping_bits = rand(block_len, 1) < 0.5;
+flipped_code = mod((coded_bits + flipping_bits), 2);
+
+sigma = 0.4;
+AWG_noise = sigma/sqrt(2)*(randn(block_len, 1)); 
+
+received_bits = flipped_code + AWG_noise;
+
+
