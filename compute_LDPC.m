8b66f0a6b5d128705106cd54851280b7d4042ff3
rquinlancmu <44323130+rquinlancmu@users.noreply.github.com>
2018-12-02 21:53:08 -0500

    Decode properly

--- compute_LDPC.m
+++ compute_LDPC.m
@@ -5,8 +5,8 @@ clear all;
 % create initial object
 LDPC_code = LDPC_class(0, 0);
 
+%half rate code
 block_len = 1944;
-rate = 1/2;
 
 LDPC_code.load_parity_matrix(block_len);
 info_len = LDPC_code.info_len;
@@ -15,10 +15,14 @@ message_bits = rand(info_len, 1) < 0.5;
 %Encode bits
 coded_bits = LDPC_code.encode_bits(message_bits);
 
+% calculate Eb/No after experimenting with some values
+No = 5;
+snr_in_db = No + 10*log10(No)
+
+
 %flip some bits, add some noise
 flipping_bits = rand(block_len, 1) < 0.5;
 flipped_code = mod((coded_bits + flipping_bits), 2);
-
 %modulate, here with BPSK, to then decode
 modulated_bits = zeros(size(flipped_code));
 for jj = 1:size(flipped_code)
@@ -29,16 +33,21 @@ for jj = 1:size(flipped_code)
     end
 end
 
-sigma = 0.4;
-AWG_noise = sigma/sqrt(2)*(randn(block_len, 1)); 
+AWG_noise = sqrt(No) * randn(1944, 1);
 
-received_bits = modulated_bits + AWG_noise;
 
-No = 0.5; %debugging number, update later with actual calculation
+
+snr_in_db = 10^(snr_in_db/10);
+received_bits = modulated_bits + AWG_noise/sqrt(snr_in_db);
 
 llr = LDPC_code.find_llr(received_bits, No);
 llr = llr .* (1 - 2*flipping_bits);
 
+[resulting_message, error_bits] = decode_bits(LDPC_code, llr, 20);
+
+num_same_bits = numel(find(resulting_message == coded_bits));
+
+ber = abs((num_same_bits - block_len) / block_len)
 
 
         
